ee55c07d918d4cde33ec9d9cfcda0bbd8fdae503
Vovchik <kaktusation@gmail.com>
2024-04-01 14:23:15 +0300

    rofi scripts added

---
+++ .rofi_scripts/theme-selector/theme-selector.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# options to be displayed
+option0="frappe"
+option1="latte"
+option2="everforest"
+# options passed into variable
+options="$option0\n$option1\n$option2"
+
+chosen="$(echo -e "$options" | rofi -lines 3 -dmenu -p "theme")"
+python3 /home/vladimir/projects/theme-switcher/main.py $chosen
