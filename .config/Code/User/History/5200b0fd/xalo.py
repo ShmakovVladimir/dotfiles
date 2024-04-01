f83605dc49407193027771b1ce12e843d9d9ed77
Vovchik <kaktusation@gmail.com>
2024-04-01 16:08:56 +0300

    some files deleted

--- .config/Code/User/History/5200b0fd/xalo.py
+++
@@ -1,7 +0,0 @@
-x = int(input("Введите число x:  "))
-y = int(input("Введите число y:  "))
-z = int(input("Введите число z:  "))
-
-
-numerator = ((x**5 + 7) / (6 * y))**(1/3) 
-denominator = 7 - z % y
